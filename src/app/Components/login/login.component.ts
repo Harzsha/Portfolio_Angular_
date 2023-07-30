dd431e7259ac028c1bac6500b1dc26618ab6553c
Harsha <harshagowtham30@gmail.com>
2023-07-30 11:29:09 +0530

    basic commit login form

--- src/app/Components/login/login.component.ts
+++ src/app/Components/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
-
+  constructor(private modalService: NgbModal) {
+  }
+  public open(modal: any): void {
+    this.modalService.open(modal);
+  }
 }
